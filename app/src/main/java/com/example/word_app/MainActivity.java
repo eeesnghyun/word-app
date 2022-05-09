d2632ee39800a717820f71356c22996f24597a5e
eeesnghyun <szhyun2002@gmail.com>
2022-05-09 23:30:46 +0900

    로딩화면 추가

--- app/src/main/java/com/example/word_app/MainActivity.java
+++ app/src/main/java/com/example/word_app/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -38,6 +39,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Loading
+        Intent intent = new Intent(this, LoadingActivity.class);
+        startActivity(intent);
 
         recyclerView = findViewById(R.id.recyclerView);
 
