617114eae16ec2ca28c30dfd5942188367f24f14
eeesnghyun <szhyun2002@gmail.com>
2022-05-15 01:46:13 +0900

    로딩 화면 추가

--- app/src/main/java/com/example/word_app/LoadingActivity.java
+++ app/src/main/java/com/example/word_app/LoadingActivity.java
@@ -1,8 +1,12 @@
 package com.example.word_app;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class LoadingActivity extends Activity {
     @Override
@@ -10,6 +14,9 @@ public class LoadingActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
         startLoading();
+
+        ImageView imageView = (ImageView)findViewById(R.id.img_loading);
+        Glide.with(this).load(R.raw.loading).into(imageView);
     }
 
     private void startLoading() {
@@ -19,6 +26,6 @@ public class LoadingActivity extends Activity {
             public void run() {
                 finish();
             }
-        }, 2000);
+        }, 4000);
     }
 }
